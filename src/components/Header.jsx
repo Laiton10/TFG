e27c69c88847dec6681ca704efcc2989123bd286
roldaan04 <javirol2005@gmail.com>
2025-04-12 20:49:46 +0200

    Mejorando el menu responsive + pequeños cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,68 +1,114 @@
 import React, { useState, useEffect } from 'react';
-import logo from '../resources/FilMe-removebg-preview.png'
-import principalData from '../data/principal.json'
+import logo from '../resources/FilMe-removebg-preview.png';
+import principalData from '../data/principal.json';
 import { Link, useLocation } from 'react-router-dom';
-import '../styles/components/Header.css'
+import '../styles/components/Header.css';
 
-function Header({user}) {
+function Header({ user }) {
   const [links, setLinks] = useState({});
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuResponsiveAbierto, setMenuResponsiveAbierto] = useState(false);
   const location = useLocation();
 
   const clickMouse = () => {
     setIsMenuOpen((prevState) => !prevState);
-  }
+  };
 
+  const toggleResponsiveMenu = () => {
+    setMenuResponsiveAbierto((prev) => !prev);
+  };
 
   useEffect(() => {
     const linkObject = principalData[0];
     setLinks(linkObject);
   }, []);
 
-
   return (
-    <div>
-        <nav className='nav'>
-          <div className='logoLinks'> 
-             <img className='logo' src={logo} alt='logo'/>
-             <ul className='nav-links'>
-            {console.log(Object.keys(links))}  
-            {Object.keys(links).map((key) => (
-              <li key={key}><Link to={`/${key}`}>{links[key]}</Link></li>
-            ))}
-          </ul>
+    <>
+      {/* Menú hamburguesa responsive */}
+      <div
+        id="responsive-nav-icon"
+        onClick={toggleResponsiveMenu}
+        className={menuResponsiveAbierto ? 'open' : ''}
+      >
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+
+      <div id="responsive-nav" className={menuResponsiveAbierto ? 'visible' : 'hidden'}>
+        <ul>
+          {Object.keys(links).map((key) => (
+            <li key={key}>
+              <Link
+                to={`/${key}`}
+                className="responsive-header-link"
+                onClick={() => setMenuResponsiveAbierto(false)}
+              >
+                {links[key]}
+              </Link>
+            </li>
+          ))}
+          {user && (
+            <li>
+              <Link
+                to="/ajustes"
+                className="responsive-header-link"
+                onClick={() => setMenuResponsiveAbierto(false)}
+              >
+                Ajustes y pedidos
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+
+      {/* Menú principal de escritorio */}
+      <div>
+        <nav className="nav">
+          <div className="logoLinks">
+            <img className="logo" src={logo} alt="logo" />
+            <ul className="nav-links">
+              {Object.keys(links).map((key) => (
+                <li key={key}>
+                  <Link to={`/${key}`}>{links[key]}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
-         
-        <div className="inicioSesion">
-        {user ? 
-          <> {/* Es necesario un fragmento, xq hay varios elementos JSX */}
-            <div className="imagenMenu"onClick={clickMouse}>
-              <img
-                src="/account.svg"
-                alt="Icono de usuario" 
-                className="iconoUsuario"
-                
-              />
-            
-            
-        {isMenuOpen &&  // Muestra el menú si isMenuOpen es true, gracias a && que evalúa la segunda parte si la primera es true
-            <div className={`menuDesplegable ${isMenuOpen ? 'open' :  ''}`}>
-              <ul>
-                <li><Link to="/perfil">Mi Perfil</Link></li>
-                <li><Link to="/configuracion">Configuración</Link></li>
-              </ul>
-            </div>
-        }
-            </div>
-          </>
- : 
-    location.pathname !== '/login' && (<Link to="/login">Iniciar Sesión</Link>
-    )
-}
-        </div>
+
+          <div className="inicioSesion">
+            {user ? (
+              <>
+                <div className="imagenMenu" onClick={clickMouse}>
+                  <img
+                    src="/account.svg"
+                    alt="Icono de usuario"
+                    className="iconoUsuario"
+                  />
+
+                  {isMenuOpen && (
+                    <div className={`menuDesplegable ${isMenuOpen ? 'open' : ''}`}>
+                      <ul>
+                        <li>
+                          <Link to="/perfil">Mi Perfil</Link>
+                        </li>
+                        <li>
+                          <Link to="/configuracion">Configuración</Link>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </>
+            ) : (
+              location.pathname !== '/login' && <Link to="/login">Iniciar Sesión</Link>
+            )}
+          </div>
         </nav>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
